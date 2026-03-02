9dfb2573e75e83a1f4e54aa102c6c1e55a3add9a
Patrick Long <longpatrick3317@gmail.com>
2026-03-02 16:12:31 -0500

    bubble done fr this time & Icontweaks in settings

--- Bind/src/screens/SettingsScreen.tsx
+++ Bind/src/screens/SettingsScreen.tsx
@@ -58,7 +58,7 @@ const TrashIcon = ({ color = '#FFFFFF' }: { color?: string }) => (
 );
 
 const MessageIcon = ({ color = '#FFFFFF' }: { color?: string }) => (
-  <BoxiconsFilled name="bx-message-circle-notification" size={iconSize.forTabs} color={color} />
+  <BoxiconsFilled name="bx-message-dots" size={iconSize.forTabs} color={color} />
 );
 
 const BugIcon = ({ color = '#FFFFFF' }: { color?: string }) => (
@@ -66,13 +66,7 @@ const BugIcon = ({ color = '#FFFFFF' }: { color?: string }) => (
 );
 
 const ShieldIcon = ({ color = '#FFFFFF' }: { color?: string }) => (
-  <Svg width={iconSize.forTabs} height={iconSize.forTabs} viewBox="0 0 24 24" fill={color}>
-    <Path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M12.516 2.17a.75.75 0 0 0-1.032 0 11.209 11.209 0 0 1-7.877 3.08.75.75 0 0 0-.722.515A12.74 12.74 0 0 0 2.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 0 0 .374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 0 0-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08Zm3.094 8.016a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-    />
-  </Svg>
+  <BoxiconsFilled name="bx-info-shield" size={iconSize.forTabs} color={color} />
 );
 
 const FileTextIcon = ({ color = '#FFFFFF' }: { color?: string }) => (
